4695f719d6e5a18d60cc093476fc9fc600edbfa3
daniel.szopa333 <daniel.szopa333@gmail.com>
2022-01-15 11:38:19 +0100

    Add-Migration DeleteFirstNameAndLastName

--- TutorWebApi.Domain/Entities/Profile.cs
+++ TutorWebApi.Domain/Entities/Profile.cs
@@ -3,8 +3,6 @@
     public class Profile
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Description { get; set; }
         public int UserRef { get; set; }
         public User User { get; set; }
