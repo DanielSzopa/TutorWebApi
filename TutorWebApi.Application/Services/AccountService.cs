fe81af9d9998cb24d9b109ce5a357c6fe7f4d0b6
daniel.szopa333 <daniel.szopa333@gmail.com>
2022-01-22 12:25:21 +0100

    Complete register function

--- TutorWebApi.Application/Services/AccountService.cs
+++ TutorWebApi.Application/Services/AccountService.cs
@@ -20,7 +20,8 @@ namespace TutorWebApi.Application
             var user = _mapper.Map<User>(registerDto);
             var password = _passwordHasher.HashPassword(user, registerDto.Password);
             user.Password = password;
-           
+            
+            await _userRepository.RegisterUser(user);
         }
     }
 }
