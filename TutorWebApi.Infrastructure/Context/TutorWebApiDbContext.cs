d73aa876d88cc63342a21faa32ff7456b51d78d0
daniel.szopa333 <daniel.szopa333@gmail.com>
2022-01-31 22:31:49 +0100

    Add extension methods for modelBuilder

--- TutorWebApi.Infrastructure/Context/TutorWebApiDbContext.cs
+++ TutorWebApi.Infrastructure/Context/TutorWebApiDbContext.cs
@@ -2,6 +2,7 @@
 using TutorWebApi.Domain.Common;
 using TutorWebApi.Domain.Entities;
 using TutorWebApi.Domain.Interfaces;
+using TutorWebApi.Infrastructure.Config;
 
 namespace TutorWebApi.Infrastructure.Context
 {
@@ -27,170 +28,16 @@ namespace TutorWebApi.Infrastructure.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Achievement>()
-                .Property(a => a.Name)
-                .HasMaxLength(150)
-                .IsRequired();
-
-            modelBuilder.Entity<Achievement>()
-                .Property(a => a.ProfilId)
-                .IsRequired();
-
-            modelBuilder.Entity<Address>()
-               .Property(a => a.Country)
-               .HasMaxLength(50)
-               .IsRequired();
-
-            modelBuilder.Entity<Address>()
-               .Property(a => a.City)
-               .HasMaxLength(50)
-               .IsRequired();
-
-            modelBuilder.Entity<Address>()
-              .Property(a => a.PosteCode)
-              .HasMaxLength(10)
-              .IsRequired();
-
-            modelBuilder.Entity<Address>()
-              .Property(a => a.Street)
-              .HasMaxLength(60);
-
-            modelBuilder.Entity<Address>()
-             .Property(a => a.AccommodationNumber)
-             .HasMaxLength(6);
-
-            modelBuilder.Entity<Address>()
-               .Property(a => a.UserRef)
-               .IsRequired();
-
-            modelBuilder.Entity<Advert>()
-               .Property(a => a.Title)
-               .HasMaxLength(150)
-               .IsRequired();
-
-            modelBuilder.Entity<Advert>()
-               .Property(a => a.City)
-               .HasMaxLength(50)
-               .IsRequired();
-
-            modelBuilder.Entity<Advert>()
-              .Property(a => a.ProfileId)
-              .IsRequired();
-
-            modelBuilder.Entity<Advert>()
-              .Property(a => a.SubjectId)
-              .IsRequired();
-
-            modelBuilder.Entity<AdvertContact>()
-              .Property(ac => ac.AdvertRef)
-              .IsRequired();
-
-            modelBuilder.Entity<AdvertContact>()
-             .Property(ac => ac.Mail)
-             .HasMaxLength(100)
-             .IsRequired();
-
-            modelBuilder.Entity<Comment>()
-              .Property(c => c.Description)
-              .IsRequired();
-
-            modelBuilder.Entity<Comment>()
-              .Property(c => c.ProfileId)
-              .IsRequired();
-
-            modelBuilder.Entity<Comment>()
-             .Property(c => c.UserId)
-             .IsRequired();
-
-            modelBuilder.Entity<Experience>()
-              .Property(e => e.ProfileId)
-              .IsRequired();
-
-            modelBuilder.Entity<Experience>()
-              .Property(e => e.Name)
-              .HasMaxLength(150)
-              .IsRequired();
-
-            modelBuilder.Entity<Like>()
-              .Property(l => l.UserId)
-              .IsRequired();
-
-            modelBuilder.Entity<Like>()
-              .Property(l => l.ProfileId)
-              .IsRequired();
-
-            modelBuilder.Entity<Profile>()
-             .Property(p => p.UserRef)
-             .IsRequired();
-
-            modelBuilder.Entity<Subject>()
-             .Property(s => s.Name)
-             .HasMaxLength(50)
-             .IsRequired();
-
-            modelBuilder.Entity<User>()
-             .Property(u => u.FirstName)
-             .HasMaxLength(25)
-             .IsRequired();
-
-            modelBuilder.Entity<User>()
-             .Property(u => u.LastName)
-             .HasMaxLength(25)
-             .IsRequired();
-
-            modelBuilder.Entity<User>()
-            .Property(u => u.Mail)
-            .HasMaxLength(100)
-            .IsRequired();
-
-            modelBuilder.Entity<User>()
-            .Property(u => u.Password)
-            .IsRequired();
-
-            modelBuilder.Entity<User>()
-            .Property(u => u.DateOfBirth)
-            .IsRequired();
-
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.Address)
-                .WithOne(a => a.User)
-                .HasForeignKey<Address>(a => a.UserRef);
-
-            modelBuilder.Entity<Advert>()
-                .HasOne(a => a.AdvertContact)
-                .WithOne(ac => ac.Advert)
-                .HasForeignKey<AdvertContact>(ac => ac.AdvertRef);
-
-            modelBuilder.Entity<Profile>()
-                .HasOne(p => p.User)
-                .WithOne(u => u.Profile)
-                .HasForeignKey<Profile>(p => p.UserRef);
-
-            modelBuilder.Entity<Comment>()
-                .HasOne(c => c.User)
-                .WithMany(u => u.Comments)
-                .HasForeignKey(c => c.UserId)
-                .OnDelete(DeleteBehavior.NoAction);
-
-            modelBuilder.Entity<Comment>()
-               .HasOne(c => c.Profile)
-               .WithMany(p => p.Comments)
-               .HasForeignKey(c => c.ProfileId);
-
-
-            modelBuilder.Entity<Like>()
-                .HasOne(l => l.User)
-                .WithMany(u => u.Likes)
-                .HasForeignKey(c => c.UserId)
-                .OnDelete(DeleteBehavior.NoAction);
-
-            modelBuilder.Entity<Like>()
-               .HasOne(l => l.Profile)
-               .WithMany(p => p.Likes)
-               .HasForeignKey(l => l.ProfileId);
-
-
+            modelBuilder.AddAchievementConfig();
+            modelBuilder.AddAddressConfig();
+            modelBuilder.AddAdvertConfig();
+            modelBuilder.AddAdvertContactConfig();
+            modelBuilder.AddCommentConfig();
+            modelBuilder.AddExperienceConfig();
+            modelBuilder.AddLikeConfig();
+            modelBuilder.AddProfileConfig();
+            modelBuilder.AddSubjectConfig();
+            modelBuilder.AddUserConfig();
         }
 
         public override async Task<int> SaveChangesAsync(CancellationToken token = new CancellationToken())
